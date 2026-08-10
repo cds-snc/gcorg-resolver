4865c6c62268ca990307810a2d30a7452d45a131
colindouglas <colin.douglas@cds-snc.ca>
2026-08-10 11:20:58 -0300

    fix: linting

--- .github/workflows/scripts/aws_cost_report.py
+++ .github/workflows/scripts/aws_cost_report.py
@@ -88,7 +88,9 @@ if __name__ == "__main__":
 
     start_date, end_date = calculate_last_week()
     raw_amount, unit = get_weekly_cost(start_date, end_date)
-    display_start_date = datetime.datetime.fromisoformat(start_date).strftime("%b %-d, %Y")
+    display_start_date = datetime.datetime.fromisoformat(start_date).strftime(
+        "%b %-d, %Y"
+    )
     display_end_date = (
         datetime.datetime.fromisoformat(end_date).date() - timedelta(days=1)
     ).strftime("%b %-d, %Y")
